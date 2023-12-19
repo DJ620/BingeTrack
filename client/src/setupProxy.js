6faab08151f87964fe517ccc25b1a8a1ac971262
DJ Hersh <DanielJHersh@gmail.com>
2023-12-18 20:12:30 -0800

    fixed typo

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     createProxyMiddleware({
       // 👇️ make sure to update your target
       // target: 'http://localhost:8000/',
-      target: "https://tv-tracker-4s31.onrender.com",
+      target: "https://tv-tracker-4s31.onrender.com/",
       changeOrigin: true,
     }),
   );
